d5430869ff4382c690ae95710c9a26bef7f209cc
piees <yngveranestad@gmail.com>
2016-12-12 12:49:16 +0100

    refactor and add comments for docker api

--- docker/main.py
+++ docker/main.py
@@ -2,45 +2,44 @@ from flask import Flask
 app = Flask(__name__)
 import subprocess
 from flask import request
+import config
+import sys
+
+if config.dockercontainer == "dockercontainer":
+    sys.exit("Set your docker container in config.py")
 
 
 @app.route('/', methods=['GET', 'POST'])
 def test():
     if request.method == 'POST':
+
+        # saves the file sent
         f = request.files['files']
         f.save('tempphoto.jpg')
-        #        subprocess.Popen("echo 'sup'", stdout=subprocess.PIPE, shell=True)
+
+        # moves the saved file into the docker container
         subprocess.Popen(
-            "docker cp tempphoto.jpg goofy_austin:/tf_files/temp/tempphoto.jpg",
+            "docker cp tempphoto.jpg {}:/tf_files/temp/tempphoto.jpg".format(
+                config.dockercontainer),
             stdout=subprocess.PIPE,
             shell=True)
-        #		return subprocess.Popen("docker exec zen_murdock python /mlapp/label_image.py /mlapp/tempphoto.jpg", stdout=subprocess.PIPE, shell=True).communicate()[0]
+
+        # runs the classifier on the file
         proc = subprocess.Popen(
-            "docker exec goofy_austin \
+            "docker exec {} \
             /tensorflow/bazel-bin/tensorflow/examples/label_image/label_image \
             --graph=/tf_files/retrained_graph.pb \
             --labels=/tf_files/retrained_labels.txt --output_layer=final_result \
-            --image=/tf_files/temp/tempphoto.jpg > process.txt",
+            --image=/tf_files/temp/tempphoto.jpg"
+            .format(config.dockercontainer),
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             shell=True).communicate()[1]
+
         return proc
     else:
         return 'Post failed'
 
 
-'''
-result = subprocess.Popen(
-    "docker exec goofy_austin \
-    /tensorflow/bazel-bin/tensorflow/examples/label_image/label_image \
-    --graph=/tf_files/retrained_graph.pb \
-    --labels=/tf_files/retrained_labels.txt --output_layer=final_result \
-    --image=/tf_files/temp/tempphoto.jpg",
-    stdout=subprocess.PIPE,
-    shell=True).communicate()[0]
-print result
-'''
-
 if __name__ == '__main__':
-    #    app.run(debug=True,host='0.0.0.0')
     app.run(host='0.0.0.0')
--- docker/send.py
+++ docker/send.py
@@ -1,11 +1,15 @@
 import requests
 import sys
+import config
+'''
+To test:
+run with file that you want classified as argument
+ex. python send.py pic.jpg
+'''
 
-#url = "http://127.0.0.1:5000/"
-url = "http://10.0.0.124:5000/"
+url = config.url
 files = {'files': open(sys.argv[1], 'rb')}
 r = requests.post(url, files=files)
-#print r.text
 
 for x in r.text.split('I tensorflow/examples/label_image/main.cc:206] ')[1:]:
     print x
