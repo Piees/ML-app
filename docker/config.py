f255a4df9585d7721521e2880b4698b283c908f1
piees <yngveranestad@gmail.com>
2016-12-15 14:04:03 +0100

    add send config

--- docker/config.py
+++ docker/config.py
@@ -1,1 +1,2 @@
 dockercontainer = "dockercontainer"
+url = "url"
--- docker/send.py
+++ docker/send.py
@@ -7,6 +7,9 @@ run with file that you want classified as argument
 ex. python send.py pic.jpg
 '''
 
+if config.url == "url":
+    sys.exit("Set your url in config.py")
+
 url = config.url
 files = {'files': open(sys.argv[1], 'rb')}
 r = requests.post(url, files=files)
